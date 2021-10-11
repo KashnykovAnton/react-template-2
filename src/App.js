7137349f050c81a9775c8be6f9c9b05900365c7f
KashnykovAnton <kashnikov_anton@yahoo.com>
2021-10-12 01:45:55 +0300

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,27 +1,9 @@
 import './App.css';
-import user from './components/Profile/user.json';
-import statData from './components/Statistics/statistical-data.json';
-import friends from './components/FriendList/friends.json';
-import transactions from './components/TransactionHistory/transactions.json';
-import { Profile } from './components/Profile/Profile';
-import { Statistics } from './components/Statistics/Statistics';
-import { FriendList } from './components/FriendList/FriendList';
-import { TransactionHistory } from './components/TransactionHistory/TransactionHistory';
 
 function App() {
   return (
-    <div className="App">
-      <Profile
-        name={user.name}
-        tag={user.tag}
-        location={user.location}
-        avatar={user.avatar}
-        stats={user.stats}
-      />
-      <Statistics title="Upload stats" stats={statData} />
-      {/* <Statistics stats={statData} />; */}
-      <FriendList friends={friends} />
-      <TransactionHistory items={transactions} />;
+    <div>
+      <h1>Welcome to React!</h1>
     </div>
   );
 }
